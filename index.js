d19990b708af8ea3f1de9dba76b66eb23f325eab
mwendamwenda <benmwenda83@gmail.com>
2023-01-02 19:49:08 -0800

    varaibles refresher

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('amazing');
+
+console.log(30);
+let fisrtName = 'Ben';
+console.log(fisrtName);
+
+let _function = 'normal';
+console.log(_function);
+
+//variable is like a box which stores  value
+//variables can be used over nad over again
+//when writing variables we use dollar sign or underscore
+//always use camle case rule whe n naming variables
