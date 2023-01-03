0bfc2add72f58fc0ad8d9dfaa7693e708e675705
mwendamwenda <benmwenda83@gmail.com>
2023-01-02 20:45:45 -0800

    new status

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 console.log('amazing');
 
 console.log(30);
@@ -11,3 +12,17 @@ console.log(_function);
 //variables can be used over nad over again
 //when writing variables we use dollar sign or underscore
 //always use camle case rule whe n naming variables
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'ben');
+
+let year;
+console.log(typeof year);
+year = 1992;
+console.log(typeof year);
